7d1ada79c847f35db8adb762dd82eea2e6739afa
dmlaziuk <dm.laziuk@gmail.com>
2017-11-23 00:44:29 +0300

    delete with auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dmlaziuk", password: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
