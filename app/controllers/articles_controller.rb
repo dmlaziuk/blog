55b9d46b80741616f6107240a8a523a73b30f385
dmlaziuk <dm.laziuk@gmail.com>
2017-11-23 00:21:58 +0300

    works with comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-    @article.save
-    redirect_to @article
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
